e95b281ba64afb909ee6496c75b0a23376caeb20
Sridevi-HD <64984414+Sridevi-HD@users.noreply.github.com>
2020-05-13 21:36:03 -0700

    Update models.py

--- design/models.py
+++ design/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
 
-class Destination(models.Model):
+PAY_MODE={
+    ("Credit_card","Credit_card"),
+    ("Debit_card","Debit_card")
 
-    name = models.CharField(max_length=100)
-    desc = models.TextField()
-    price = models.IntegerField()
-    offer = models.BooleanField(default=False)
+}
+class Bookingdetails(models.Model):
+    objects=models.Manager()
+    first_name=models.CharField(max_length=20)
+    user_name=models.CharField(max_length=30,primary_key=True)
+    phone_no=models.BigIntegerField()
+    address1=models.CharField(max_length=100)
+    
+
+    paymode=models.CharField(choices=PAY_MODE,max_length=30)
+    cc_name=models.CharField(max_length=10)
+    cc_number=models.IntegerField()
+    cc_experation=models.CharField(max_length=20)
+    cc_cvv=models.IntegerField()
